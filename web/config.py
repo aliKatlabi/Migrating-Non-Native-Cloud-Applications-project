3e3bc2b7844e2a6144e23b2abd0bda724aa3e006
aliKatlabi <a2222k@gmail.com>
2025-03-10 18:07:25 +0100

    requirements and configs changes
    requirements and configs changes

--- web/config.py
+++ web/config.py
@@ -4,14 +4,14 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=""  #TODO: Update value
-    POSTGRES_USER="" #TODO: Update value
-    POSTGRES_PW=""   #TODO: Update value
-    POSTGRES_DB=""   #TODO: Update value
+    POSTGRES_URL="projectapp-postgresql.postgres.database.azure.com" 
+    POSTGRES_USER="projectappdb"
+    POSTGRES_PW= os.getenv('POSTGRES_PW')  #in .env file 
+    POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
-    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
